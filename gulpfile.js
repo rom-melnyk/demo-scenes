d438f2b786c8977bef7efe1143e3a13abf2b8c20
Roman Melnyk <email.rom.melnyk@gmail.com>
2019-02-03 21:01:56 +0100

    Update Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,48 +17,64 @@ const DEST_DIR = `demo/js/`;
 const minify = composer(uglify, console);
 
 
-/** @subtasks-group clean */
-gulp.task('clean:js', () => del([ `${DEST_DIR}${DEST_NAME}*` ]));
+function taskCleanJS() {
+  return del([ `${DEST_DIR}${DEST_NAME}*` ]);
+}
 
 
-/** @subtasks-group JS */
-gulp.task('js:dev', [ 'clean:js' ],  () => {
-    const b = browserify({
-        entries: ENTRY,
-        debug: true
-    });
+function taskJSDev() {
+  const b = browserify({
+    entries: ENTRY,
+    debug: true
+  });
 
-    return b.bundle()
-            .on('error', gutil.log)
-        .pipe(source(DEST_NAME))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(DEST_DIR));
-});
+  return b.bundle()
+    .on('error', gutil.log)
+    .pipe(source(DEST_NAME))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DEST_DIR));
+}
 
-gulp.task('js:prod', [ 'clean:js' ],  () => {
-    const b = browserify({
-        entries: ENTRY
-    });
+function taskJSWatch(done) {
+  gulp.watch([ 'src/**/*' ], () => {
+    gulp.series(
+      taskCleanJS,
+      taskJSDev
+    );
+  });
+  done();
+}
 
-    return b.bundle()
-            .on('error', gutil.log)
-        .pipe(source(DEST_NAME))
-        .pipe(buffer())
-        .pipe(minify())
-            .on('error', gutil.log)
-        .pipe(gulp.dest(DEST_DIR));
-});
+function taskJSProd() {
+  const b = browserify({
+    entries: ENTRY
+  });
 
+  return b.bundle()
+    .on('error', gutil.log)
+    .pipe(source(DEST_NAME))
+    .pipe(buffer())
+    .pipe(minify())
+    .on('error', gutil.log)
+    .pipe(gulp.dest(DEST_DIR));
+}
 
-/** @tasks-group */
-gulp.task('dev', [ 'js:dev' ], () => {
-    gulp.watch('src/**/*', [ 'js:dev' ]);
-});
+// ---------------------------------------
 
-gulp.task('prod', [ 'js:prod' ]);
+exports.dev = gulp.series(
+  taskCleanJS,
+  taskJSDev,
+  taskJSWatch
+);
 
-gulp.task('default', () => {
-    console.log('\n\tUse `gulp dev` or `gulp prod`.\n');
-});
+exports.prod = gulp.series(
+  taskCleanJS,
+  taskJSProd
+);
+
+exports.default = function _default(done) {
+  console.log('\n\tUse `gulp dev` or `gulp prod`.\n');
+  done();
+};
